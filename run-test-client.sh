b7210cf5d2fba43d6f3e561ed4805a7d1bc3386c
David Neiman <david_neiman@harvard.edu>
2016-04-15 15:26:17 -0400

    Update documentation.

--- run-test-client.sh
+++ run-test-client.sh
@@ -7,4 +7,4 @@ TEST_CLASSPATH=$FITS_SERVICE_HOME:$FITS_SERVICE_HOME/WebContent/WEB-INF/classes:
 $FITS_SERVICE_HOME/WebContent/WEB-INF/lib/log4j-1.2.17.jar:$FITS_SERVICE_HOME/WebContent/WEB-INF/lib/commons-logging-1.2.jar:\
 $FITS_SERVICE_HOME/lib/httpclient-4.5.2.jar:$FITS_SERVICE_HOME/lib/httpclient-cache-4.5.2.jar:$FITS_SERVICE_HOME/lib/httpcore-4.4.4.jar:$FITS_SERVICE_HOME/lib/httpmime-4.5.2.jar
 
-java -cp $TEST_CLASSPATH edu.harvard.hul.ois.fits.clients.FormFileUploaderClientApplication $1
+java -cp $TEST_CLASSPATH edu.harvard.hul.ois.fits.clients.FormFileUploaderClientApplication $1 $2
